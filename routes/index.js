fe853584af30001fd6bf07aed35336fa6bace984
katrinajamir <katrinajamir@gmail.com>
2022-04-28 02:33:14 -0700

    added login skeleton, added photos, created scss file

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Hello!' });
+  res.render('home', { title: 'My Books - Welcome!' });
 });
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Hello!' });
+});
+
 
 module.exports = router;
