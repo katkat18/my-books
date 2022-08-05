546e5ef6fa9d52874b300ed8d20f7d219219a941
katrinajamir <katrinajamir@gmail.com>
2022-08-05 02:28:41 -0700

    added book page and add book link

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('home', { title: 'My Books - Welcome!' });
 });
 
-/* GET login page. */
+/* GET login form page. */
 router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Hello!' });
 });
@@ -21,9 +21,19 @@ router.get('/profile', function(req, res, next) {
   res.render('profile', { title: 'User' });
 });
 
-/* GET signup page */
+/* GET signup form page */
 router.get('/signup', function(req, res, next) {
   res.render('signup', { title: 'Sign Up!' });
+});
+
+/* GET book page */
+router.get('/book', function(req, res, next) {
+  res.render('book', { title: 'Book' });
+});
+
+/* GET book page */
+router.get('/addBook', function(req, res, next) {
+  res.render('addBook', { title: 'Add Book' });
 });
 
 module.exports = router;
