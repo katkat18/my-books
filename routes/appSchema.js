e729a655ea5d5b6723ee772a0ccb4b415db5c563
katrinajamir <katrinajamir@gmail.com>
2022-08-25 21:28:34 -0700

    added mongoose schema and simple signup

---
+++ routes/appSchema.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://katrinajamir:koolkatbooks@katjamjam.b7qkwy7.mongodb.net/?retryWrites=true&w=majority");
+
+const connection = mongoose.connection;
+
+connection
+.on('open', () => console.log("database connected"))
+.on('close', () => console.log("database disconnected"))
+.on('error', (error) => console.log(error));
+
+const {model, Schema} = mongoose;
+
+const userSchema = new Schema({
+    username: {type: String, unique:true, required:true},
+    password: {type: String, required: true},
+    profileimg: {data: Buffer, contentType: String},
+    books: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Book'
+    }]  
+}, {timstamps: true});
+
+const bookSchema = new Schema({
+    title: String,
+    author: String,
+    rating: Number,
+    comment: String,
+    bookimg: {data: Buffer, contentType: String},
+    creator: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, {timeStamps: true});
+
+exports.User = model('User', userSchema);
+exports.Book = model('Books', bookSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const mongoose = require('mongoose');
+const schema = require('./appSchema');
+const User = schema.User;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -34,6 +38,31 @@ router.get('/book', function(req, res, next) {
 /* GET book page */
 router.get('/addBook', function(req, res, next) {
   res.render('addBook', { title: 'Add Book' });
+});
+
+/* POST user signup */
+/*Simple sign up. Will add validation later */
+router.post('/user-signup', (req, res) => {
+  console.log(`username: ${req.body.username}`);
+  console.log(`password: ${req.body.password}`);
+  console.log(`confirm password: ${req.body.confirm_password}`);
+  
+  const username = req.body.username;
+  const password = req.body.password;
+  const cPassword = req.body.confirm_password;
+  
+  const newUser = new User(req.body);
+
+
+  newUser.save()
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+  res.redirect('/signup');
 });
 
 module.exports = router;
