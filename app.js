f4d3e1daa7f048d3b7940cbb81fc31b1d75c044f
katrinajamir <katrinajamir@gmail.com>
2022-08-30 00:11:26 -0700

    login with passport

--- app.js
+++ app.js
@@ -3,11 +3,90 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
+
+//passport authentication configuration 
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const mongoose = require('mongoose');
+const schema = require('./routes/appSchema');
+const User = schema.User;
+
+mongoose.connect("mongodb+srv://katrinajamir:koolkatbooks@katjamjam.b7qkwy7.mongodb.net/?retryWrites=true&w=majority");
+
+const connection = mongoose.connection;
+
+connection
+.on('open', () => console.log("database connected"))
+.on('close', () => console.log("database disconnected"))
+.on('error', (error) => console.log(error));
+
+
+app.use(session({
+  secret: 'kit kats',
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({mongoUrl: "mongodb+srv://katrinajamir:koolkatbooks@katjamjam.b7qkwy7.mongodb.net/?retryWrites=true&w=majority"}),
+  cookie: { secure: false }
+}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(
+  (username, password, cb) => {
+    console.log("in local strategy");
+    User.findOne({username: username}, (err, user) => {
+      console.log(`to authenticate user: ${user.username}`);
+      if(err) { 
+        console.log(`there is an error: ${err}`);
+        return cb(err); 
+      }
+      console.log("checking no user");
+      if(!user) {
+        console.log("no user found for authenticate");
+        return cb(null, false); 
+      }
+      console.log("checking password");
+      if(user.password != password){ 
+        console.log("password does not match");
+        return cb(null, false); 
+      }
+      console.log(`Local strategy success for user: ${user}`);
+      return cb(null, user);
+    });
+  }
+));
+
+//sessions
+passport.serializeUser((user, cb) => {
+  console.log(`in serializer user id: ${user.id}`);
+  cb(null,user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  console.log(`in deserialize user id: ${id}`);
+  User.findById(id, (err, user) => {
+    cb(err, user);
+  });
+});
+
+/*
+app.use((req,res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated();
+  req.next();
+});
+*/
+app.set('trust proxy', 1);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -1,42 +1,55 @@
 const express = require('express');
 const router = express.Router();
 
+const passport = require('passport');
+
 const mongoose = require('mongoose');
 const schema = require('./appSchema');
 const User = schema.User;
 
+/* checks if user is still logged in  */
+function isLoggedIn(req, res, next) {
+  console.log(`user is authenticated? ${req.isAuthenticated()}`);
+  if(req.isAuthenticated()) {
+    console.log("user is logged in");
+    return next();
+  }else
+  console.log("user is not logged in");
+  res.redirect('/login');
+}
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('home', { title: 'My Books - Welcome!' });
 });
 
 /* GET login form page. */
-router.get('/login', function(req, res, next) {
+router.get('/login', (req, res, next) => {
   res.render('login', { title: 'Hello!' });
 });
 
 /* GET main page (after login) */
-router.get('/main', function(req, res, next) {
-  res.render('main', { title: 'Welcome User' });
+router.get('/main', isLoggedIn, (req, res, next) => {
+  res.render('main', { title: `Welcome User`});
 });
 
 /* GET profile page (after login) */
-router.get('/profile', function(req, res, next) {
+router.get('/profile', (req, res, next) => {
   res.render('profile', { title: 'User' });
 });
 
 /* GET signup form page */
-router.get('/signup', function(req, res, next) {
+router.get('/signup', (req, res, next) => {
   res.render('signup', { title: 'Sign Up!' });
 });
 
 /* GET book page */
-router.get('/book', function(req, res, next) {
+router.get('/book', (req, res, next) => {
   res.render('book', { title: 'Book' });
 });
 
 /* GET book page */
-router.get('/addBook', function(req, res, next) {
+router.get('/addBook', (req, res, next) => {
   res.render('addBook', { title: 'Add Book' });
 });
 
@@ -64,5 +77,26 @@ router.post('/user-signup', (req, res) => {
 
   res.redirect('/signup');
 });
+
+/* POST user login */
+router.post('/user-login', passport.authenticate('local', {
+    //successRedirect: '/main',
+    failureRedirect: '/login',
+    failureMessage: "failed to login"
+  }), (req, res) => {
+    res.redirect('/main');
+  });
+
+/* POST logout */
+router.post('/logout', (req, res) => {
+  req.logout((err) => {
+    if(err) {
+      console.log("failed to logout");
+      return next(err);
+    }
+    console.log("logging out");
+    res.redirect('/');
+  })
+})
 
 module.exports = router;
